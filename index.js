ca361ab9612d9d0d8cdc455eae3e086e916bbe09
yjh2848 <yunj05763@gmail.com>
2023-12-05 10:12:59 +0900

    fix : env파일 생성

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const app = express();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "dbswhdgur2843!",
-  database: "yun",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err);
     throw err;
   }
   console.log("MySQL Connected...");
